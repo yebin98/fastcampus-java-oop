57342d7557f7a71a424643a490ea9c80b1a54770
yebin <limyebin1218@naver.com>
2024-04-13 03:46:34 +0900

    Design #3 - Sort interface
    인터페이스 설계하고, 각 구현체가 인터페이스를 구현하는 구조로 수정

--- src/main/java/logic/JavaSort.java
+++ src/main/java/logic/JavaSort.java
@@ -11,7 +11,7 @@ import java.util.List;
  * date           : 2024-04-13
  * description    :
  */
-public class JavaSort <T extends Comparable<T>> {
+public class JavaSort <T extends Comparable<T>> implements Sort<T>{
 
     public List<T> sort(List<T> list) {
         List<T> output = new ArrayList<>(list);
